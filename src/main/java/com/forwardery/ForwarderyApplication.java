c5488f1bee00226b219e06836bfcf0a9fce97de3
hadi-nasiri-1368 <hadi.nnasiri@yahoo.com>
2026-06-25 14:09:41 +0330

    first commit

---
+++ src/main/java/com/forwardery/ForwarderyApplication.java
@@ -0,0 +1,13 @@
+package com.forwardery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForwarderyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForwarderyApplication.class, args);
+	}
+
+}
